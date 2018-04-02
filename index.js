dfc33eae3e3a04d836372e7f38998c33f009b523
herris suhendra <herris@growthsteel.com>
2018-04-03 01:58:46 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 const _ = require('underscore')
 const key = 'AIzaSyATGbyfcvW-uanT-Imwayj8KjDg_iHJgIs'
 const googleMapsClient = require('@google/maps').createClient({
